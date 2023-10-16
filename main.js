977fad35d7ededff89c27bd18291490ade492c01
thina1698 <145748647+thina1698@users.noreply.github.com>
2023-10-16 11:25:29 +0530

    Update main.js
    Calculator JS

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Calculator
 const keys = document.querySelectorAll('.key');
 const display_input = document.querySelector('.display .input');
 const display_output = document.querySelector('.display .output');
